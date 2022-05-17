927590c5bc2f6b0df0b198bcc466bbd9449a61f5
Sawicki Rafal <sawickirafal@outlook.com>
2022-05-17 02:13:12 +0200

    Change message-event

--- src/functions/bot/message-event.js
+++ src/functions/bot/message-event.js
@@ -2,17 +2,11 @@ const {botApp, awsLambdaReceiver} = require("../../helpers/aws-slack-bot");
 const appsRegistry = require('../../helpers/apps-registry')
 
 botApp.event('message', async ({event, say,message}) => {
-    if (event.text === '!hello') {
-        await say(`Hey there <@${message.user}>!`);
-    } else if (event.text === '!goodbye') {
-        await say(`See ya later, <@${message.user}> :wave:`);
-    }
     await appsRegistry.notifyAllBy("MESSAGE", event);
 });
 
 // Handle the Lambda function event
 module.exports.handler = async (event, context, callback) => {
     const handler = await awsLambdaReceiver.start();
-    context.callbackWaitsForEmptyEventLoop = false;
     return handler(event, context, callback);
 }
