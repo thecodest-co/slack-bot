3e2d135bc0162ac9697267641e27849028133e84
Sawicki Rafal <sawickirafal@outlook.com>
2022-06-03 02:29:35 +0200

    Add constants

---
+++ src/config/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+    events:{
+      MESSAGE_EVENT: 'message',
+      TEAM_JOIN_EVENT: 'team_join'
+    },
+    dbEvents: {
+        MESSAGE_EVENT: `MESSAGE`,
+        TEAM_JOIN_EVENT: 'TEAM_JOIN'
+    }
+}
