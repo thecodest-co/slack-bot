95974918379cd90ee4252896905967824dc5fe96
Sawicki Rafal <sawickirafal@outlook.com>
2022-06-03 02:28:28 +0200

    Add welcome for new user feature

---
+++ src/config/messages.js
@@ -0,0 +1,14 @@
+module.exports = {
+    getWelcomeMessageForNewUser: (userId) => `Hi <@${userId}>! :pepe-dance2:
+
+Great to see you have joined our awesome team at The Codest!
+I'm Cody, and I'm here as I only hang out with cool kids :cool-dodge:.
+
+You probably wonder what else do I do? There you go:
+- I'm a master networker, helping others to get to know each other :man-woman-girl-boy:
+- I'm a lucky chipmunk as it's obvious every great team should have at least one chipmunk :chipmunk:
+- I'm still a rookie, but learning new things damn fast! :congaparrot:
+Enjoy your ride with us and remember to never stop being cool. :sunglasses:
+
+Peace.:peace-out:`
+}
---
+++ src/tasks/schedule-welcome-for-new-user.js
@@ -0,0 +1,26 @@
+const {botApp} = require("../helpers/aws-slack-bot");
+const {getWelcomeMessageForNewUser} = require("../config/messages");
+
+async function scheduleWelcomeForNewUser(user) {
+    const message = getWelcomeMessageForNewUser(user.id);
+    //scheduleMessage requires integer of unix epoch in seconds
+    const roundedScheduleDateInUnixEpoch = Math.round(getScheduleDate().getTime() / 1000);
+    await botApp.client.chat.scheduleMessage({
+        channel: user.id,
+        text: message,
+        post_at: roundedScheduleDateInUnixEpoch
+    });
+}
+
+function getScheduleDate() {
+    const date = new Date();
+    const delayDays = process.env.WELCOME_MESSAGE_DELAY_DAYS || 0;
+    const delayHours = process.env.WELCOME_MESSAGE_DELAY_HOURS || 0;
+    const delayMinutes = process.env.WELCOME_MESSAGE_DELAY_MINUTES || 1;
+    const delayInMillis = (((delayDays * 24) + delayHours) * 60 + delayMinutes) * 60 * 1000;
+    return new Date(date.getTime() + delayInMillis);
+}
+
+module.exports = {
+    scheduleWelcomeForNewUser
+}
